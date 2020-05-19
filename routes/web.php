2ec83eecc9723631891e7ae5c89cda710d992b45
cjs <942403648@qq.com>
2020-05-19 22:02:25 +0800

    邮箱认证

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
 
-Auth::routes();
+Auth::routes([
+    'verify' => true
+]);
