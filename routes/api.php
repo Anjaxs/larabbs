628e0621358c5b39877d272d4627e3887dfd5148
cjs <942403648@qq.com>
2020-09-07 23:43:27 +0800

    发布话题

--- routes/api.php
+++ routes/api.php
@@ -57,6 +57,11 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
 
             // 上传图片
             Route::post('images', 'ImagesController@store')->name('images.store');
+
+            // 发布话题
+            Route::resource('topics', 'TopicsController')->only([
+                'store', 'update', 'destroy',
+            ]);
         });
     });
 });
